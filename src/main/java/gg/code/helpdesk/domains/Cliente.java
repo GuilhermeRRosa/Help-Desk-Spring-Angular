123f00c2111812ed0590aa07486843b6c583ca1e
Guilherme R Rosa <ggcodigos@gmail.com>
2023-08-26 00:35:47 -0300

    JPA Annotations added

--- src/main/java/gg/code/helpdesk/domains/Cliente.java
+++ src/main/java/gg/code/helpdesk/domains/Cliente.java
@@ -1,17 +1,26 @@
 package gg.code.helpdesk.domains;
 
+import gg.code.helpdesk.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Cliente extends Pessoa{
 
+    @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Cliente() {
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 }
